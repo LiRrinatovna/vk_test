03f67b222de7f5e8b5869b394c1233885f138e7b
Alina_Khovanova <lirinatovna2@gmail.com>
2023-08-09 10:07:56 +0500

    фыв

--- src/panels/Catalog.js
+++ src/panels/Catalog.js
@@ -1,29 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import {Icon24Filter, Icon28SearchOutline, Icon28BookSpreadOutline, Icon28ListBulletSquareOutline, Icon28UserStarBadgeOutline} from '@vkontakte/icons';
-import {Panel, Search, Header, PanelHeaderBack, Group, PanelHeader, Tabbar, TabbarItem, Counter} from '@vkontakte/vkui';
+import { Icon24Filter, Icon28SearchOutline, Icon28BookSpreadOutline, Icon28ListBulletSquareOutline, Icon28UserStarBadgeOutline } from '@vkontakte/icons';
+import { Panel, Search, Header, PanelHeaderBack, Group, PanelHeader, Tabbar, TabbarItem, Counter } from '@vkontakte/vkui';
 import './custom.css';
 
-function Content(der){
+function Content(der) {
     der = 0
-    if(der = 0){
-        return <p slideWidth="30%">Серии</p>
+    if (der = 0) {
+        return <Header slideWidth="30%">Серии</Header>
     }
-    if(der = 1){
-        return<p slideWidth="30%">Книги</p>
+    if (der = 1) {
+        return <Header slideWidth="30%">Книги</Header>
     }
-    
+
 }
 const Catalog = props => (
 
-        <Content />
+    <Content />
 
 );
 
 Catalog.propTypes = {
-	id: PropTypes.string.isRequired,
-	go: PropTypes.func.isRequired,
+    id: PropTypes.string.isRequired,
+    go: PropTypes.func.isRequired,
 };
 
 export default Catalog;
